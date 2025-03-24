14f9ce8aa20fe0260381d271a2dd5476ae293fe1
sammed.sankonatti <sammed.sankonatti18@gmail.com>
2025-03-24 13:11:27 +0530

    Deploy final time

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
-      <h1> NextJs deployment on AWS Amplify</h1>
+      <h1>NextJs deployment on App service</h1>
     </div>
   );
 }
